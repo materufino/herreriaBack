ce5d396591678d8ec4d88d2d57b759586d3b0dd5
MiquelAbellaG <miquel.abella.dw@immune.institute>
2023-03-08 22:21:04 +0100

    se añaden más valores al token de login

--- backherreria/helpers/utils.js
+++ backherreria/helpers/utils.js
@@ -4,7 +4,11 @@ const jwt = require('jsonwebtoken');
 const createToken = (user) => {
     const obj = {
         user_id: user.id,
+        user_name: user.name,
+        user_surname: user.surname,
+        username: user.username,
         user_rango: user.rango,
+        user_category: user.category,
         exp: dayjs().add(90, 'days').unix()
     }
     return jwt.sign(obj, 'nisi electi praeteribit');
